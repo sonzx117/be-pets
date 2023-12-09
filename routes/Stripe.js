fef59d35491927bf8a402ba77001a686fa97d8ee
intern.2023.tranhoangson <hoangsonzx77@gmail.com>
2023-12-09 09:08:21 +0700

    fix be

--- routes/Stripe.js
+++ routes/Stripe.js
@@ -1,8 +1,77 @@
-module.exports = (app) => {
-    var Stripe = require("../controller/Stripe");
-    var router = require("express").Router();
+// module.exports = (app) => {
+//     var Stripe = require("../controller/Stripe");
+//     var router = require("express").Router();
 
-    router.post("/", Stripe.payment);
+//     router.post("/", Stripe.payment);
 
-    app.use("/payment", router);
-};
+//     app.use("/payment", router);
+// };
+// module.exports = (app) => {
+//     var Stripe = require("../controller/Stripe");
+//     var router = require("express").Router();
+
+//     router.post("/", Stripe.payment);
+
+//     app.use("/payment", router);
+// };
+const express = require('express');
+const Stripe = require('stripe');
+// const app = express();
+require('dotenv').config();
+const stripe = Stripe(process.env.STRIPE_KEY);
+
+const router = express.Router();
+
+router.post('/create-checkout-session', async (req, res) => {
+  const { listCart } = req.body;
+
+  if (!listCart || !Array.isArray(listCart) || listCart.length === 0) {
+    return res.status(400).send({ error: 'Invalid or empty listCart' });
+  }
+
+  console.log('listCart:', listCart);
+
+  const line_items = listCart.map((item) => {
+    // Convert priceResult to xu (cents for VND)
+    // const unitAmountInXu = item.priceResult * 100;
+    return {
+      price_data: {
+        currency: 'vnd',
+        product_data: {
+          name: item.name,
+          images: [item.avatar],
+          metadata: {
+            id: item.id,
+          },
+        },
+        unit_amount: item.priceResult,
+      },
+      quantity: item.quantityCurrent,
+    };
+  });
+
+  try {
+    const session = await stripe.checkout.sessions.create({
+      line_items,
+      mode: 'payment',
+      success_url: 'http://localhost:13000/Shop/payment-success',
+      cancel_url: 'http://localhost:13000/Shop',
+    });
+
+    res.send({ url: session.url });
+  } catch (error) {
+    console.error('Error creating checkout session:', error);
+    res.status(500).send({ error: 'Internal Server Error' });
+  }
+
+  // const session = await stripe.checkout.sessions.create({
+  //   line_items,
+  //   mode: 'payment',
+  //   success_url: 'http://localhost:13000/Shop',
+  //   cancel_url: 'http://localhost:13000',
+  // });
+
+  // res.send({ url: session.url });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,51 +1,99 @@
-var express = require("express");
-var bodyParser = require("body-parser");
+// var express = require("express");
+// var bodyParser = require("body-parser");
+// var app = express();
+// var cors = require("cors");
+// require("dotenv").config();
+// var options = {
+//   origin: "*",
+//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//   preflightContinue: false,
+//   optionsSuccessStatus: 204,
+// };
+// app.use(cors(options));
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.get("/", (req, res) => {
+//   res.send(
+//     "<h1>Chào tất cả các bạn đến với api dịch vụ thú cưng của tôi!</h1>"
+//   );
+// });
+// require("./routes/Tag")(app);
+// require("./routes/Schedule")(app);
+// require("./routes/Bill")(app);
+// require("./routes/TagNew")(app);
+// require("./routes/New")(app);
+// require("./routes/User")(app);
+// require("./routes/Role")(app);
+// require("./routes/Weight")(app);
+// require("./routes/Service")(app);
+// require("./routes/Gallery")(app);
+// require("./routes/Contact")(app);
+// require("./routes/Pet")(app);
+// require("./routes/SocialNetwork")(app);
+// require("./routes/Login")(app);
+// require("./routes/ImgPet")(app);
+// require("./routes/category")(app);
+// require("./routes/Product")(app);
+// require("./routes/UserRole")(app);
+// require("./routes/TagProduct")(app);
+// require("./routes/ImageProduct")(app);
+// require("./routes/Shop")(app);
+// require("./routes/Stripe")(app);
+
+// app.use(function (err, req, res, next) {
+//   res.status(500).send(err);
+// });
+
+// app.listen(process.env.PORT || 555, () => {
+//   console.log(
+//     `Chào mừng bạn đến với Backend http://localhost:${process.env.PORT}`
+//   );
+// });
+var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
-var cors = require("cors");
-require("dotenv").config();
+var cors = require('cors');
+const stripe = require('./routes/Stripe');
+require('dotenv').config();
 var options = {
-  origin: "*",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   preflightContinue: false,
   optionsSuccessStatus: 204,
 };
 app.use(cors(options));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.get("/", (req, res) => {
-  res.send(
-    "<h1>Chào tất cả các bạn đến với api dịch vụ thú cưng của tôi!</h1>"
-  );
+app.get('/', (req, res) => {
+  res.send('<h1>Chào tất cả các bạn đến với api dịch vụ thú cưng của tôi!</h1>');
 });
-require("./routes/Tag")(app);
-require("./routes/Schedule")(app);
-require("./routes/Bill")(app);
-require("./routes/TagNew")(app);
-require("./routes/New")(app);
-require("./routes/User")(app);
-require("./routes/Role")(app);
-require("./routes/Weight")(app);
-require("./routes/Service")(app);
-require("./routes/Gallery")(app);
-require("./routes/Contact")(app);
-require("./routes/Pet")(app);
-require("./routes/SocialNetwork")(app);
-require("./routes/Login")(app);
-require("./routes/ImgPet")(app);
-require("./routes/category")(app);
-require("./routes/Product")(app);
-require("./routes/UserRole")(app);
-require("./routes/TagProduct")(app);
-require("./routes/ImageProduct")(app);
-require("./routes/Shop")(app);
-require("./routes/Stripe")(app);
-
+require('./routes/Tag')(app);
+require('./routes/Schedule')(app);
+require('./routes/Bill')(app);
+require('./routes/TagNew')(app);
+require('./routes/New')(app);
+require('./routes/User')(app);
+require('./routes/Role')(app);
+require('./routes/Weight')(app);
+require('./routes/Service')(app);
+require('./routes/Gallery')(app);
+require('./routes/Contact')(app);
+require('./routes/Pet')(app);
+require('./routes/SocialNetwork')(app);
+require('./routes/Login')(app);
+require('./routes/ImgPet')(app);
+require('./routes/category')(app);
+require('./routes/Product')(app);
+require('./routes/UserRole')(app);
+require('./routes/TagProduct')(app);
+require('./routes/ImageProduct')(app);
+require('./routes/Shop')(app);
+// require('./routes/Stripe')(app);
 app.use(function (err, req, res, next) {
   res.status(500).send(err);
 });
+app.use('/api/stripe', stripe);
 
 app.listen(process.env.PORT || 555, () => {
-  console.log(
-    `Chào mừng bạn đến với Backend http://localhost:${process.env.PORT}`
-  );
+  console.log(`Chào mừng bạn đến với Backend http://localhost:${process.env.PORT}`);
 });
